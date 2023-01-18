2e30edf70db2986f6e98e8bdcb85d7903fb12bed
Linzh <catmo1999@gmail.com>
2023-01-18 21:38:48 +0200

    update: p1 fin

--- P1-MatrixMultiplication/p1.py
+++ P1-MatrixMultiplication/p1.py
@@ -1,28 +1,42 @@
-import random
 import numpy as np
 from memory_profiler import profile
-import matplotlib.pyplot as plt
 
 
+# get matirx with size 1e6 * 1e3
 def get_A():
     return np.random.rand(int(1e6), int(1e3))
 
 
+# get matirx with size 1e3 * 1e6
 def get_B():
     return np.random.rand(int(1e3), int(1e6))
 
 
+# get matirx with size 1e6 * 1
 def get_C():
     return np.random.rand(int(1e6), 1)
 
 
-# record memory usage
+# use this function if your computer has enough memory (large than 16GB RAM, maybe 32GB)
+# @profile
+# def compute():
+#     # step 1
+#     A = get_A()
+#     B = get_B()
+#     C = get_C()
+#     # step 2
+#     BC = np.dot(B, C)
+#     # step 3
+#     return np.dot(A, BC)
+
+
+# otherwise, use this function
 @profile
 def compute():
-    # step 1
+    # step 1, compute B*C first
     BC = np.dot(get_B(), get_C())
-    # step 2
+    # step 2, compute A * (B * C)
     return np.dot(get_A(), BC)
 
-
+# let's go
 ABC = compute()
